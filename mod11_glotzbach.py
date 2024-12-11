07f75b9cdfa6cb3e429e79b9079a859ecd049c59
jglotzbach <jason.glotzbach@hsc.utah.edu>
2024-12-10 17:52:13 -0700

    Create mod11_glotzbach.py

---
+++ mod11_glotzbach.py
@@ -0,0 +1,11 @@
+#PyBrain is an open-source library for python that is used for machine learning. 
+#It allows the user to build neural networks to train and test algorithms to deal with datasets. 
+#It interfaces with Mathplotlib for data visualizations. 
+#The advantages include use of creation of simple feed forward networks, it’s ease of use, and modular design. 
+#Limitations include slow performance with larger data sets and more complex data sets.
+
+!pip install pybrain
+!pip install numpy
+!pip install scipy
+
+from pybrain.structure import FeedForwardNetwork
